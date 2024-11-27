d56f8e1c0b414b778e25c39bbebdab4a0e323a91
BrunnoBernardes <brunnobernardes@discente.ufg.br>
2024-11-27 09:45:20 -0300

    Estudos 11.27

---
+++ ip-listas/Lista_03-EstruturasEAlocacaoDinamica/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ct, n_fracoes, verificador = 0;
+    int i, j;
+    char *OP = NULL;
+
+    scanf ("%d", &ct);
+
+
+    return 0;
+}
